f97fb2d44629822e81dd46c55866572b4088422d
Weezya <nguema@et.esiea.fr>
2021-08-04 01:19:13 +0200

    Site en construction

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <AppBar/>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import AppBar from './components/AppBar';
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    AppBar,
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/AppBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-app-bar
+      app
+      color="white"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink"
+          contain
+          :src="require('../assets/logoce2.png')"
+          transition="scale-transition"
+          width="106"
+        />
+      </div>
+    </v-app-bar>
+</template>
